be4cfb794e3551a9e02d94abda1f3b034cf25c84
islam-solaiman <islam.osama.ed@gmail.com>
2023-06-06 17:34:54 -0700

    0x01. Python - if/else, loops, functions - TASK 10

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-add = __import__('10-add').add
+def add(a, b):
+    return a + b
 
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
+if __name__ == '__main__':
+    print(add(1, 2))
+    print(add(98, 0))
+    print(add(100, -2))
