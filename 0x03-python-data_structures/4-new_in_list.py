4142a534d23d2a42befcc4f5f431ad42cbb45ca7
islam-solaiman <islam.osama.ed@gmail.com>
2023-06-11 15:19:58 -0700

    0x03. Python - Data Structures: Lists, Tuples - TASK 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,7 @@
 def new_in_list(my_list, idx, element):
     if idx > len(my_list) - 1 or idx < 0:
         return (m_list)
+
     new_list = my_list.copy()
     new_list[idx] = element
     return (new_list)
