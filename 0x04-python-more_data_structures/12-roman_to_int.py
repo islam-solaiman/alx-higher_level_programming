1fc17e924c6353b6cdd676aa3b95ad849f5b2a5b
islam-solaiman <islam.osama.ed@gmail.com>
2023-06-14 16:17:44 -0700

    0x04. Python - More Data Structures: Set, Dictionary - TASK 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
+    if roman_string is None or type(roman_string) is not str:
+        return (0)
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
     for i in range(len(roman_string)):
-        result += rom[roman_string[i]]
-        if i + 1 < len(roman_string) and rom[roman_string[i]] <\
-           rom[roman_string[i + 1]]:
-            result -= rom[roman_string[i]] * 2
-    return result
+        if i > 0 and rom[roman_string[i]] > rom[roman_string[i - 1]]:
+            num += rom[roman_string[i]] - 2 * rom[roman_string[i - 1]]
+        else:
+            num += rom[roman_string[i]]
+    return (num)
