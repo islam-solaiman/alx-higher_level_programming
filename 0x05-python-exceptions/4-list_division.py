588af381b3c8d25d0f12fcf860ffe83503334e2b
put username <islam.osama.ed@gmail.com>
2023-06-26 15:26:20 -0700

    0x05. Python - Exceptions - TASK 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         result = 0
         try:
-            result = my_list_1[i] / my_list_2[index]
+            result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print('division by 0')
         except TypeError:
