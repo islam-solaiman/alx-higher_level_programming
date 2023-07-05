dcce2bd44dbb43b0ae470902c02f1c754e2887b1
put username <islam.osama.ed@gmail.com>
2023-07-05 13:33:31 -0700

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defines a Rectangle Class"""
+"""Defines a Rectangle class."""
+
 
 class Rectangle:
-    """Represent a Rectangle"""
+    """Represent a rectangle."""
     pass
