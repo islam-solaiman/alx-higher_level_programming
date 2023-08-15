25f128210ad8f36bb524c7bc63c8010dd1e03bc4
Islam Solaiman <islam.solaiman92@gmail.com>
2023-08-15 14:38:17 -0700

    0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- script inserts new row in first_table (database hbtn_0c_0).
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, `Best School`);
+INSERT INTO `first_table` VALUES (89, 'Best School');
