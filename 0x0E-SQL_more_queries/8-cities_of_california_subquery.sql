bd4d2272dc7a0f426579ce2f2a41752eb8160d9f
Islam Solaiman <islam.solaiman92@gmail.com>
2023-08-16 16:38:34 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,3 @@
 -- script that lists all cities contained in the database hbtn_0d_usa.
--- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
 SELECT id, name FROM cities where state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
