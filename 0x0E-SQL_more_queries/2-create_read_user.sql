d9cc47e2e7c5ffb36144506e874e30b96ecf38ac
Islam Solaiman <islam.solaiman92@gmail.com>
2023-08-16 16:48:00 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,3 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
 GRANT ALL ON *.* TO user_0d_2@localhost;
+FLUSH PRIVILEGES;
