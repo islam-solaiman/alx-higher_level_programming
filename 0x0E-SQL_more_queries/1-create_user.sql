a660dcc80ff91750e57ccd621ed86d2ac2a1b0f2
Islam Solaiman <islam.solaiman92@gmail.com>
2023-08-16 16:09:50 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 --  creates the MySQL server user user_0d_1.
 -- user_0d_1 should have all privileges on your MySQL server
 -- user_0d_1 password should be set to user_0d_1_pwd
-CREATE USER IF NOT EXISTS user_0d_1@localhos IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhos' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
