93f3aa4db01214a134dc31123ab450ada42e5d1e
islam solaiman <islam.solaiman92@gmail.com>
2023-09-11 10:33:46 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-let lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let line = 0; line < lines.length; line++)
     console.log(lines[line]);
