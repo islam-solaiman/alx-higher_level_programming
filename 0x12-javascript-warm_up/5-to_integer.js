ea4421aec86f1cba6f54b44b76f6aa98e0b7f3e6
islam solaiman <islam.solaiman92@gmail.com>
2023-09-11 09:59:51 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number:', + (process.argv[2]));
+  console.log('My number:', parseInt(process.argv[2]));
 }
