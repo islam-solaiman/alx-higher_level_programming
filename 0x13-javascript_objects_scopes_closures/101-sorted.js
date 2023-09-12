3cf45a25dfbe95a5ca08f21760cbbf981c1e3015
islam solaiman <islam.solaiman92@gmail.com>
2023-09-12 16:20:50 -0700

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,12 +4,12 @@ const dict = require('./101-data').dict;
 
 const totalList = Object.entries(dict);
 const val = Object.values(dict);
-const valUniq = [...new Set(vals)];
+const valUniq = [...new Set(val)];
 const newDict = {};
 for (const i in valUniq) {
   const list = [];
   for (const j in totalList) {
-    if (totalList[j][1] === valsUniq[i]) {
+    if (totalList[j][1] === valUniq[i]) {
       list.unshift(totalList[j][0]);
     }
   }
