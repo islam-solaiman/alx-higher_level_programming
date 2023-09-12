2e42bedbe2475d6b3b87db3ab58acb05e5621b16
islam solaiman <islam.solaiman92@gmail.com>
2023-09-12 16:26:28 -0700

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const fisrtSource = require('firstSource');
+const fisrtSource = require('fisrtSource');
 
-const firstArg = firstSource.readFileSync(process.argv[2]).toString();
-const secondArg = firstSource.readFileSync(process.argv[3]).toString();
+const firstArg = fisrtSource.readFileSync(process.argv[2]).toString();
+const secondArg = fisrtSource.readFileSync(process.argv[3]).toString();
 firstSource.writeFileSync(process.argv[4], firstArg + secondArg);
