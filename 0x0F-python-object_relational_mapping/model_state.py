06402c40181e0375534f0a45837d92ec9a6fd71e
islam solaiman <islam.solaiman92@gmail.com>
2023-09-15 14:21:42 -0700

    0x0F. Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+File contains the class definition of a State and an 
+instance Base = declarative_base()
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, MetaData
+
+my_metadata = MetaData()
+Base = declarative_base(metadata=my_metadata)
+
+
+class State(Base):
+    
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
