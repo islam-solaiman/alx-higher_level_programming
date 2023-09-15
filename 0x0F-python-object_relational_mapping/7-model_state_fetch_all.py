7e60de4aace67353a7b17779f5f20f54cf0a54cd
islam solaiman <islam.solaiman92@gmail.com>
2023-09-15 14:39:51 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,17 +5,19 @@ script that lists all State objects from the database
 hbtn_0e_6_usa
 """
 
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    db_ur = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_ur)
     Session = sessionmaker(bind=engine)
+
     session = Session()
+
     for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+        print('{0}: {1}'.format(instance.id, instance.name))
