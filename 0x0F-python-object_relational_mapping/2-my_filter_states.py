0aa156cf629fc689f5fff1e2c147cc066a15731b
islam solaiman <islam.solaiman92@gmail.com>
2023-09-15 14:10:08 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     curr = db.cursor()
     curr.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-            .format(sys.argv[4]))
+                .format(sys.argv[4]))
     rows = curr.fetchall()
     for row in rows:
         print(row)
