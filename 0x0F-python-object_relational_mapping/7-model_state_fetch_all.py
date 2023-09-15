16e66abcae2b95ee0fbb6f47a1b3082277deb80d
islam solaiman <islam.solaiman92@gmail.com>
2023-09-15 14:31:22 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,6 +14,7 @@ from sqlalchemy import (create_engine)
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in session.query(State).order_by(State.id):
