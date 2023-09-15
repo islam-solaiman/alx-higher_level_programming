6d7f3da3ee45dd29a372709565bd104bd7b81676
islam solaiman <islam.solaiman92@gmail.com>
2023-09-15 14:13:46 -0700

    0x0F. Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""  lists all states from the database hbtn_0e_0_usa """
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curr = db.cursor()
+    match = sys.argv[4]
+    curr.execute("SELECT * FROM states WHERE name \
+                    LIKE %s", (match, ))
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
+    curr.close()
+    db.close()
