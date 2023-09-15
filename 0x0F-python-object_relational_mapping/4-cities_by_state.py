347bea560ac3f4cebb2a0d38b42259adbdee9411
islam solaiman <islam.solaiman92@gmail.com>
2023-09-15 14:15:55 -0700

    0x0F. Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""  lists all states from the database hbtn_0e_0_usa """
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curr = db.cursor()
+    curr.execute("""SELECT cities.id, cities.name, states.
+                name FROM cities INNER JOIN states ON
+                states.id=cities.state_id""")
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
+    curr.close()
+    db.close()
