168974e8f02f329860a2a463bfe540e817ef667b
islam solaiman <islam.solaiman92@gmail.com>
2023-09-16 14:14:27 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  script that prints the first State object 
+"""  script that prints the first State object
     from the database hbtn_0e_6_usa
 """
 
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     instance = session.query(State).order_by(State.id).first()
-    
+
     if instance is None:
         print("Nothing")
     else:
