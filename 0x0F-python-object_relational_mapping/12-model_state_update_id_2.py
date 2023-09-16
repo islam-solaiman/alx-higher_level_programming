dc80f15d664755f3f11c453380d02430332a1d1f
islam solaiman <islam.solaiman92@gmail.com>
2023-09-16 14:35:25 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,7 +9,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-        """ Updates a State object on the database."""
+        """ 
+            Updates a State object on the database.
+        """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
