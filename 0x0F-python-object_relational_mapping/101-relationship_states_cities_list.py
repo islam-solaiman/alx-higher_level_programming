d2d18a6c1b1c3e01962f33af903f2026d816c6d4
islam solaiman <islam.solaiman92@gmail.com>
2023-09-17 14:21:43 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    
+ 
     session = Session()
-    
+
     for instance in session.query(State).order_by(State.id):
         print(instance.id, instance.name, sep=": ")
         for city_in in instance.cities:
