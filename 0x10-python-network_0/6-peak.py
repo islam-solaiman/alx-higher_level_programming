cfa2b4df6abc7cb4a86f7aefb0ff3f0da45762b3
islam solaiman <islam.solaiman92@gmail.com>
2023-09-28 14:44:24 -0700

    0x10. Python - Network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,16 +2,15 @@
 """  function that finds a peak in a list of unsorted integers """
 
 
-
 def find_peak(list_of_integers):
     """finds peak in list of unsorted integers"""
     ls = list_of_integers
-    l = len(ls)
-    if l == 0:
+    le = len(ls)
+    if le == 0:
         return
-    m = l // 2
-    if (m == l - 1 or ls[m] >= ls[m + 1]) and (m == 0 or ls[m] >= ls[m - 1]):
+    m = le // 2
+    if (m == le - 1 or ls[m] >= ls[m + 1]) and (m == 0 or ls[m] >= ls[m - 1]):
         return ls[m]
-    if m != l - 1 and ls[m + 1] > ls[m]:
+    if m != le - 1 and ls[m + 1] > ls[m]:
         return find_peak(ls[m + 1:])
     return find_peak(ls[:m])
