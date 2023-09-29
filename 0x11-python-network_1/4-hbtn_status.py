caa604e3a6cf0815ca06a45cc880b62bb74dc060
islam solaiman <islam.solaiman92@gmail.com>
2023-09-29 15:41:00 -0700

    0x11. Python - Network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """ Script that fetches https://alx-intranet.hbtn.io/status """
 
-
-import requests
-
-
-if __name__ == "__main__":
-    req = requests.get("https://intranet.hbtn.io/status")
+if __name__ == '__main__':
+    import requests
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    text = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
