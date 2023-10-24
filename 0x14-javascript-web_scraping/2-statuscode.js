a0fb7b5fa487562f3247057a7b64647c98420752
islam solaiman <islam.solaiman92@gmail.com>
2023-10-24 13:44:49 -0700

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode);
 });
